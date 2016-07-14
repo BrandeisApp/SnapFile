af17b8d546ed24f4eceebd0983ed74589b453c0e
Adam <adam@adamfly.me>
2016-07-14 11:34:45 -0400

    :sparkles: Commited AWS!
    ^_^

--- server/startup.js
+++ server/startup.js
@@ -2,3 +2,9 @@ Meteor.startup(function(){
 	// put code here that you need to initialize your app
 	// the first time you run it, e.g. creating fake data for testing
 });
+S3.config = {
+    key: 'AKIAJMHKTSGHFVGDKW3Q',
+    secret: 'W0qYucZXZMcU2EbmhW8VPjei86qE4EJPGLz3hTiQ',
+    bucket: 'snapfile',
+    region: 'us-standard'
+};
