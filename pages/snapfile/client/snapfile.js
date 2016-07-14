f399d3ed8a84aa93027fb81ac1998c564cd9bb3e
Adam <adam@adamfly.me>
2016-07-14 16:10:06 -0400

    DB changes

--- pages/snapfile/client/snapfile.js
+++ pages/snapfile/client/snapfile.js
@@ -1,18 +1,50 @@
-Template.s3_tester.events({
-    "click button.upload": function(){
-        var files = $("input.file_bag")[0].files
+Images.deny({
+ insert: function(){
+ return false;
+ },
+ update: function(){
+ return false;
+ },
+ remove: function(){
+ return false;
+ },
+ download: function(){
+ return false;
+ }
+ });
 
-        S3.upload({
-                files:files,
-                path:"subfolder"
-            },function(e,r){
-                console.log(r);
+Images.allow({
+ insert: function(){
+ return true;
+ },
+ update: function(){
+ return true;
+ },
+ remove: function(){
+ return true;
+ },
+ download: function(){
+ return true;
+ }
+});
+Template.s3Tester.events({
+   'change .myFileInput': function(event, template) {
+      FS.Utility.eachFile(event, function(file) {
+        Images.insert(file, function (err, fileObj) {
+          if (err){
+             // handle error
+          } else {
+             // handle success depending what you need to do
+            var userId = Meteor.userId();
+            var imagesURL = {
+              "profile.image": "/cfs/files/images/" + fileObj._id
+            };
+            Meteor.users.update(userId, {$set: imagesURL});
+          }
         });
-    }
-})
-
-Template.s3_tester.helpers({
-    "files": function(){
-        return S3.collection.find();
-    }
-})
+     });
+   }
+});
+if (Meteor.isServer) {
+    Meteor.publish("images", function(){ return Images.find(); });
+};
