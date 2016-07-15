439b80b1ddca854f26950026bca0f23b30f1e8e7
Jonathan Parker <jonathanparker@hillelyeshiva.org>
2016-07-15 09:26:59 -0400

    friendlist

--- pages/add friend/client/addfriend.js
+++ pages/add friend/client/addfriend.js
@@ -1,7 +1,7 @@
 Template.addfriend.events({
 	'click #add':function(){
 		var friendusername = $("#friendusername").val()
-		Friends.insert({userId:Meteor.userId(), friendId:Profiles.find({username:friendusername}).fetch()[0], username:Profiles.find})
-		Friends.insert({})
+		Friends.insert({userId:Meteor.userId(), friendId:Profile.find({username:friendusername}).fetch()[0]._id, username:Profile.find({userId:Meteor.userId()}).fetch()[0].username, friendusername:Profile.find({username:friendusername}).fetch()[0].username, friendfullname:Profile.find({username:friendusername}).fetch()[0].fullname })
+		Friends.insert({friendIdId:Meteor.userId(), userId:Profile.find({username:friendusername}).fetch()[0]._id, friendusernamename:Profile.find({userId:Meteor.userId()}).fetch()[0].username, username:Profile.find({username:friendusername}).fetch()[0].username, friendfullname:Profile.find({userId:Meteor.userId()}).fetch()[0].fullname })
 	}
 })
