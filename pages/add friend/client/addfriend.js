c6cde4c44981c2c3ef8cd9898e26c48db5d315d3
jplayer1127 <jordan@Jordans-MacBook-Pro.local>
2016-07-15 11:48:59 -0400

    friend system overhaul

--- pages/add friend/client/addfriend.js
+++ pages/add friend/client/addfriend.js
@@ -1,7 +1,23 @@
 Template.addfriend.events({
 	'click #add':function(){
-		var friendusername = $("#friendusername").val()
-		Friends.insert({userId:Meteor.userId(), friendId:Profile.find({username:friendusername}).fetch()[0].userId})
-		Friends.insert({friendId:Meteor.userId(), userId:Profile.find({username:friendusername}).fetch()[0].userId})
+		var friendcode= $("#friendId").val()
+		var friendid = Profile.find({friendcode:friendcode}).fetch()
+		if(friendid.length ==1) {
+		friendid=friendid[0].userId
+		console.log(friendid)
+		if (Friends.find({userId:Meteor.userId(), friendId:friendid}).fetch().length==0){
+			
+		Friends.insert({userId:Meteor.userId(), friendId:friendid})
+		Friends.insert({friendId:Meteor.userId(), userId:friendid})
+		Router.go("/friendslist")
+	}
+	else {
+		alert('You are already friends with this user.')
+	}
+	}
+	else {
+		
+		alert('invalid code');
+	}
 	}
 })
--- pages/profile/client/profile.js
+++ pages/profile/client/profile.js
@@ -71,6 +71,14 @@ Template.profile.helpers({
 	},
 	lastname:function(){
 		return Profile.find({userId:Meteor.userId()}).fetch()[0].lastname
+	},
+	userId:function(){
+		var userId = Meteor.userId()
+		userId = userId.split("")
+		userId[5]='`'
+		userId= userId.join("")
+
+		return userId.split("`")[0];
 	}
 
 })
