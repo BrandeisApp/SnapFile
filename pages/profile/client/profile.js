3cd41723609e17bbda9363d5ba1951a8a7c0f0d2
jplayer1127 <jordan@Jordans-MacBook-Pro.local>
2016-07-14 21:31:31 -0400

    first name last name profile

--- pages/profile/client/profile.js
+++ pages/profile/client/profile.js
@@ -1,7 +1,7 @@
 
 Session.set("caneditusername",false);
-
-
+Session.set("caneditfirstname",false);
+Session.set("caneditlastname",false);
 Template.profile.rendered= function() {
 	Meteor.call("profileinsert",Meteor.userId());
 }
@@ -17,6 +17,30 @@ Template.profile.events({
 		toinsert.username= $("#newusername").val()
 		Profile.update({_id:toinsert._id},toinsert)
 		Session.set("caneditusername",false)
+	},
+	'click #editfirstname':function(){
+		
+		Session.set("caneditfirstname",true);
+		
+	},
+
+	'click #donefirstname':function(){
+		var toinsert = Profile.find({userId:Meteor.userId()}).fetch()[0]
+		toinsert.firstname= $("#newfirstname").val()
+		Profile.update({_id:toinsert._id},toinsert)
+		Session.set("caneditfirstname",false)
+	},
+	'click #editlastname':function(){
+		
+		Session.set("caneditlastname",true);
+		
+	},
+
+	'click #donelastname':function(){
+		var toinsert = Profile.find({userId:Meteor.userId()}).fetch()[0]
+		toinsert.lastname= $("#newlastname").val()
+		Profile.update({_id:toinsert._id},toinsert)
+		Session.set("caneditlastname",false)
 	}
 
 
@@ -30,7 +54,20 @@ Template.profile.helpers({
 	},
 	caneditusername:function(){
 		return Session.get("caneditusername");
+	},
+	caneditfirstname:function(){
+		return Session.get("caneditfirstname");
+	},
+	caneditlastname:function(){
+		return Session.get("caneditlastname");
+	},
+	firstname:function(){
+		return Profile.find({userId:Meteor.userId()}).fetch()[0].firstname
+	},
+	lastname:function(){
+		return Profile.find({userId:Meteor.userId()}).fetch()[0].lastname
 	}
+
 })
 Template.profile.events({
 
